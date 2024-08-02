536dfd4b921c892ac080849f31890f990ea1c20f
krushnadora010 <148646394+krushnadora010@users.noreply.github.com>
2024-08-02 16:49:55 +0530

    Create abstraction1.py

---
+++ abstraction1.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def move(self):
+        print("this is a abstraction method")
+
+class Human(Animal):
+    def move(self):
+        print("i can walk and run")
+
+class Snake(Animal):
+    def move(self):
+        print("i can crawl")
+
+class Dog(Animal):
+    def move(self):
+        print("i can bark")
+
+class lion(Animal):
+    def move(self):
+        print("i can roar")
+
+h= Human()
+h.move()
+
+s=Snake()
+s.move()
+
+d= Dog()
+d.move()
+
+l=lion()
+l.move()
+
+#a=Animal()
+#a.move()
+
+
+
+
+               
